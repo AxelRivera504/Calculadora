538b1198a0db86b91fc68ae7c2ec45852d526496
arrivera <125411077+AxelRivera504@users.noreply.github.com>
2024-07-12 02:50:03 -0600

    Primer commit sobre el proyecto de la calculadora

---
+++ Calculad/src/calculadora/Main.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package calculadora;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author arrivera
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Calculadora calcular = new Calculadora();
+        calcular.setBackground(Color.GRAY);
+        calcular.setForeground(Color.GRAY);
+        calcular.setVisible(true);
+    }
+    
+}
